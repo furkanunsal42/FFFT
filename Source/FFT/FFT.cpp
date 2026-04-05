3c7422ed02d42fc0b66c49984233190133bc0bc2
Furkan <89701935+furkanunsal42@users.noreply.github.com>
2026-04-06 01:18:52 +0300

    dft

--- Source/FFT/FFT.cpp
+++ Source/FFT/FFT.cpp
@@ -2,30 +2,47 @@
 
 //std::filesystem::path shader_directory::ffft2_shader_directory = "../FFFT/Source/GLSL/FFT/";
 
-void FFFT2::set_source_type(data_type data_type){
-	if (source_type == data_type) return;
-	source_type = data_type;
-	shaders_are_compiled = false;
+std::string FFFT2::component_to_string(component component)
+{
+	switch (component) {
+		case x: return "x";
+		case y: return "y";
+		case z: return "z";
+		case xy: return "xy";
+		case yz: return "yz";
+		case xz: return "xz";
+		case xyz: return "xyz";
+	}
+
+	std::cout << "[FFFT Error] component_to_string() is called but given compoenent is invalid" << std::endl;
+	ASSERT(false);
+	return "";
 }
 
-void FFFT2::set_source_color_texture_format(Texture2D::ColorTextureFormat color_texture_format){
-	if (source_texture_format == color_texture_format) return;
-	source_texture_format = color_texture_format;
-	shaders_are_compiled = false;
-}
-
-
-void FFFT2::set_target_type(data_type data_type){
-	if (target_type == data_type) return;
-	target_type = data_type;
-	shaders_are_compiled = false;
-}
-
-void FFFT2::set_target_color_texture_format(Texture2D::ColorTextureFormat color_texture_format){
-	if (target_texture_format == color_texture_format) return;
-	target_texture_format = color_texture_format;
-	shaders_are_compiled = false;
-}
+//void FFFT2::set_source_type(data_type data_type){
+//	if (source_type == data_type) return;
+//	source_type = data_type;
+//	shaders_are_compiled = false;
+//}
+//
+//void FFFT2::set_source_color_texture_format(Texture2D::ColorTextureFormat color_texture_format){
+//	if (source_texture_format == color_texture_format) return;
+//	source_texture_format = color_texture_format;
+//	shaders_are_compiled = false;
+//}
+//
+//
+//void FFFT2::set_target_type(data_type data_type){
+//	if (target_type == data_type) return;
+//	target_type = data_type;
+//	shaders_are_compiled = false;
+//}
+//
+//void FFFT2::set_target_color_texture_format(Texture2D::ColorTextureFormat color_texture_format){
+//	if (target_texture_format == color_texture_format) return;
+//	target_texture_format = color_texture_format;
+//	shaders_are_compiled = false;
+//}
 
 Texture2D::ColorTextureFormat FFFT2::complex_texture_format(Texture2D::ColorTextureFormat real_texture){
 	switch (real_texture) {
--- Source/FFT/FFT_Templated.h
+++ Source/FFT/FFT_Templated.h
@@ -25,59 +25,6 @@ inline bool FFFT2::is_same(T& source, T& target)
 	return source.id == target.id;
 }
 
-template<typename T>
-inline std::vector<std::pair<std::string, std::string>> FFFT2::generate_shader_macros()
-{
-	return std::vector<std::pair<std::string, std::string>>(
-		{
-			{ "ffft_source_format", T::ColorTextureFormat_to_OpenGL_compute_Image_format(source_texture_format)},
-			{ "ffft_target_format", T::ColorTextureFormat_to_OpenGL_compute_Image_format(target_texture_format)},
-
-			{ "source_image", T::ColorTextureFormat_to_OpenGL_compute_Image_type(source_texture_format)},
-			{ "target_image", T::ColorTextureFormat_to_OpenGL_compute_Image_type(target_texture_format)},
-			
-			{ "source_image_dimensionality", T::get_texture_dimention()},
-			{ "target_image_dimensionality", T::get_texture_dimention()},
-
-			{ "source_number_type", T::ColorTextureFormat_channels(source_texture_format) == 1 ? "real" : "complex"},
-			{ "target_number_type", T::ColorTextureFormat_channels(target_texture_format) == 1 ? "real" : "complex"},
-		}
-		);
-}
-
-template<typename T>
-inline void FFFT2::compile_shaders() {
-
-	if (shaders_are_compiled) return;
-
-	auto macros = generate_shader_macros();
-	
-	auto macros_x = macros;
-	auto macros_y = macros;
-	auto macros_z = macros;
-
-	macros_x.push_back({ "ffft_axis", "axis_x" });
-	macros_y.push_back({ "ffft_axis", "axis_y" });
-	macros_z.push_back({ "ffft_axis", "axis_z" });
-
-	auto macros_copy_real			= macros;
-	auto macros_copy_complex		= macros;
-	auto macros_copy_real_complex	= macros;
-
-	macros_copy_real		.push_back({ "copy_operation", "real"		 });
-	macros_copy_complex		.push_back({ "copy_operation", "complex" 	 });
-	macros_copy_real_complex.push_back({ "copy_operation", "real_complex"}); 
-
-	cp_dft_x = std::make_shared<ComputeProgram>(Shader(shader_directory::ffft2_shader_directory / "dft.comp"), macros_x);
-	cp_dft_y = std::make_shared<ComputeProgram>(Shader(shader_directory::ffft2_shader_directory / "dft.comp"), macros_y);
-	cp_dft_z = std::make_shared<ComputeProgram>(Shader(shader_directory::ffft2_shader_directory / "dft.comp"), macros_z);
-
-	cp_copy_real			= std::make_shared<ComputeProgram>(Shader(shader_directory::ffft2_shader_directory / "copy.comp"), macros_copy_real);
-	cp_copy_complex			= std::make_shared<ComputeProgram>(Shader(shader_directory::ffft2_shader_directory / "copy.comp"), macros_copy_complex);
-	cp_copy_real_complex	= std::make_shared<ComputeProgram>(Shader(shader_directory::ffft2_shader_directory / "copy.comp"), macros_copy_real_complex);
-
-	shaders_are_compiled = true;
-}
 
 namespace {
 	template<typename texture_type, typename vector_type>
