6f1e7f74a6c34be38d80bb5fcbc0507167861f4c
Furkan <89701935+furkanunsal42@users.noreply.github.com>
2026-04-06 07:44:21 +0300

    pad

--- Source/FFT/FFT.h
+++ Source/FFT/FFT.h
@@ -54,16 +54,16 @@ public:
 	template<typename T> void				fft		(T&	source,	T& target, fft_dimension dimension = default_fft_dimension<T>(), fft_algorithm algorithm = radix235_fft, glm::ivec3 offset = glm::ivec3(0), glm::ivec3 size = glm::ivec3(0));
 	template<typename T> void				i_fft	(T&	source,	T& target, fft_dimension dimension = default_fft_dimension<T>(), fft_algorithm algorithm = radix235_fft, glm::ivec3 offset = glm::ivec3(0), glm::ivec3 size = glm::ivec3(0));
 	
-	template<typename T> void				shift	(T&	source,	T& target, glm::ivec3 shift_size, fft_dimension dimension = default_fft_dimension<T>(), glm::ivec3 offset = glm::ivec3(0), glm::ivec3 size = glm::ivec3(0));
-	template<typename T> void				i_shift	(T& source, T& target, glm::ivec3 shift_size, fft_dimension dimension = default_fft_dimension<T>(), glm::ivec3 offset = glm::ivec3(0), glm::ivec3 size = glm::ivec3(0));
+	template<typename T> void				shift	(T&	source,	T& target, glm::ivec3 shift_size);
+	template<typename T> void				i_shift	(T& source, T& target, glm::ivec3 shift_size);
 
 	template<typename T> void				copy	(T& source,	T& target, component comp = real_complex, glm::ivec3 source_offset = glm::ivec3(0), glm::ivec3 target_offset = glm::ivec3(0), glm::ivec3 size = glm::ivec3(0));
 	template<typename T> std::shared_ptr<T> create	(T& source, component comp = real_complex, glm::ivec3 size = glm::ivec3(0));
 
 	template<typename T> void				pad		(T& source, T& target, glm::vec2 padding_value = glm::vec2(0), glm::ivec3 offset = glm::ivec3(0));
 	template<typename T> void				i_pad	(T& source, T& target, glm::ivec3 offset = glm::ivec3(0));
-	template<typename T> std::shared_ptr<T> pad		(T& source, component comp = real_complex, glm::ivec3 padding_size = glm::ivec3(0), glm::vec2 padding_value = glm::vec2(0), glm::ivec3 offset = glm::ivec3(0), glm::ivec3 size = glm::ivec3(0));
-	template<typename T> std::shared_ptr<T> i_pad	(T& source, component comp = real_complex, glm::vec2 padding_value = glm::vec2(0), glm::ivec3 padding_size = glm::ivec3(0), glm::ivec3 offset = glm::ivec3(0), glm::ivec3 size = glm::ivec3(0));
+	template<typename T> std::shared_ptr<T> pad		(T& source, glm::vec2 padding_value = glm::vec2(0), glm::ivec3 offset = glm::ivec3(0));
+	template<typename T> std::shared_ptr<T> i_pad	(T& source, glm::ivec3 offset, glm::ivec3 size);
 
 	//void window();
 	//void inverse_window();
@@ -72,12 +72,12 @@ public:
 	//void divide();
 	//void add();
 	//void subtract();
-	
+
+private:
+
 	template<typename T> static bool is_complex(T& source);
 	template<typename T> static bool is_real(T& source);
 	template<typename T> static bool is_same(T& source, T& target);
-
-private:
 
 	std::string component_to_string(component component);
 
@@ -87,44 +87,20 @@ private:
 	template<typename T> void dft		(T& source, T& target, fft_dimension dimension = default_fft_dimension<T>(), glm::ivec3 offset = glm::ivec3(0), glm::ivec3 size = glm::ivec3(0));
 	template<typename T> void radix2	(T& source, T& target, fft_dimension dimension = default_fft_dimension<T>(), glm::ivec3 offset = glm::ivec3(0), glm::ivec3 size = glm::ivec3(0));
 	template<typename T> void radix3	(T& source, T& target, fft_dimension dimension = default_fft_dimension<T>(), glm::ivec3 offset = glm::ivec3(0), glm::ivec3 size = glm::ivec3(0));
+	template<typename T> void radix4	(T& source, T& target, fft_dimension dimension = default_fft_dimension<T>(), glm::ivec3 offset = glm::ivec3(0), glm::ivec3 size = glm::ivec3(0));
 	template<typename T> void radix5	(T& source, T& target, fft_dimension dimension = default_fft_dimension<T>(), glm::ivec3 offset = glm::ivec3(0), glm::ivec3 size = glm::ivec3(0));
+	template<typename T> void radix6	(T& source, T& target, fft_dimension dimension = default_fft_dimension<T>(), glm::ivec3 offset = glm::ivec3(0), glm::ivec3 size = glm::ivec3(0));
+	template<typename T> void radix7	(T& source, T& target, fft_dimension dimension = default_fft_dimension<T>(), glm::ivec3 offset = glm::ivec3(0), glm::ivec3 size = glm::ivec3(0));
+	template<typename T> void radix8	(T& source, T& target, fft_dimension dimension = default_fft_dimension<T>(), glm::ivec3 offset = glm::ivec3(0), glm::ivec3 size = glm::ivec3(0));
+	template<typename T> void radix9	(T& source, T& target, fft_dimension dimension = default_fft_dimension<T>(), glm::ivec3 offset = glm::ivec3(0), glm::ivec3 size = glm::ivec3(0));
+	template<typename T> void radix10	(T& source, T& target, fft_dimension dimension = default_fft_dimension<T>(), glm::ivec3 offset = glm::ivec3(0), glm::ivec3 size = glm::ivec3(0));
+	template<typename T> void radix11	(T& source, T& target, fft_dimension dimension = default_fft_dimension<T>(), glm::ivec3 offset = glm::ivec3(0), glm::ivec3 size = glm::ivec3(0));
+	template<typename T> void radix16	(T& source, T& target, fft_dimension dimension = default_fft_dimension<T>(), glm::ivec3 offset = glm::ivec3(0), glm::ivec3 size = glm::ivec3(0));
+	template<typename T> void radix25	(T& source, T& target, fft_dimension dimension = default_fft_dimension<T>(), glm::ivec3 offset = glm::ivec3(0), glm::ivec3 size = glm::ivec3(0));
 	
 	VariantedComputeProgram cp_dft;
 	VariantedComputeProgram cp_copy;
 };
 
 #include "FFT_Templated.h"
-
-template<typename texture_type, typename vector_type>
-void clear_texture_glm(texture_type& texture, vector_type offset, vector_type size, glm::vec4 color) = delete;
-
-template<> void clear_texture_glm(Texture1D& texture, glm::ivec3 offset, glm::ivec3 size, glm::vec4 color) {
-	texture.clear(color, offset.x, size.x, 0);
-}
-template<> void clear_texture_glm(Texture2D& texture, glm::ivec3 offset, glm::ivec3 size, glm::vec4 color) {
-	texture.clear(color, offset.x, offset.y, size.x, size.y, 0);
-}
-template<> void clear_texture_glm(Texture2DArray& texture, glm::ivec3 offset, glm::ivec3 size, glm::vec4 color) {
-	texture.clear(color, offset.x, offset.y, offset.z, size.x, size.y, size.z, 0);
-}
-template<> void clear_texture_glm(Texture3D& texture, glm::ivec3 offset, glm::ivec3 size, glm::vec4 color) {
-	texture.clear(color, offset.x, offset.y, offset.z, size.x, size.y, size.z, 0);
-}
-
-template<typename T>
-inline void FFFT2::pad(T& source, T& target, glm::vec2 padding_value, glm::ivec3 offset)
-{
-	if (glm::any(glm::lessThan(target.get_size(), source.get_size()))) {
-		ASSERT(false);
-	}
-
-	if (glm::any(glm::greaterThan(to_ivec3(source.get_size()) + offset, to_ivec3(target.get_size())))) {
-		ASSERT(false);
-	}
-
-
-	clear_texture_glm(target, glm::ivec3(0), to_ivec3(target.get_size()), glm::vec4(padding_value, 0, 1));
-	copy()
-
-}
 
--- Source/FFT/FFT_Templated.h
+++ Source/FFT/FFT_Templated.h
@@ -55,13 +55,22 @@ inline bool FFFT2::is_same(T& source, T& target)
 }
 
 namespace {
-	template<typename texture_type, typename vector_type>
-	std::shared_ptr<texture_type> create_texture_glm(vector_type size, Texture2D::ColorTextureFormat format) = delete;
 
-	template<> std::shared_ptr<Texture1D> create_texture_glm(glm::ivec1 size, Texture2D::ColorTextureFormat format) {
+	glm::ivec3 to_ivec3(glm::ivec2 size2, int32_t blank_value = 0) {
+		return glm::ivec3(size2.x, size2.y, blank_value);
+	}
+
+	glm::ivec3 to_ivec3(glm::ivec1 size1, int32_t blank_value = 0) {
+		return glm::ivec3(size1.x, blank_value, blank_value);
+	}
+
+	template<typename texture_type>
+	std::shared_ptr<texture_type> create_texture_glm(glm::ivec3 size, Texture2D::ColorTextureFormat format) = delete;
+
+	template<> std::shared_ptr<Texture1D> create_texture_glm(glm::ivec3 size, Texture2D::ColorTextureFormat format) {
 		return std::make_shared<Texture1D>(size.x, format, 1, 0);
 	}
-	template<> std::shared_ptr<Texture2D> create_texture_glm(glm::ivec2 size, Texture2D::ColorTextureFormat format) {
+	template<> std::shared_ptr<Texture2D> create_texture_glm(glm::ivec3 size, Texture2D::ColorTextureFormat format) {
 		return std::make_shared<Texture2D>(size.x, size.y, format, 1, 0);
 	}
 	template<> std::shared_ptr<Texture2DArray> create_texture_glm(glm::ivec3 size, Texture2D::ColorTextureFormat format) {
@@ -71,12 +80,20 @@ namespace {
 		return std::make_shared<Texture3D>(size.x, size.y, size.z, format, 1, 0);
 	}
 
-	glm::ivec3 to_ivec3(glm::ivec2 size2, int32_t blank_value = 0) {
-		return glm::ivec3(size2.x, size2.y, blank_value);
-	}
+	template<typename texture_type, typename vector_type>
+	void clear_texture_glm(texture_type& texture, vector_type offset, vector_type size, glm::vec4 color) = delete;
 
-	glm::ivec3 to_ivec3(glm::ivec1 size1, int32_t blank_value = 0) {
-		return glm::ivec3(size1.x, blank_value, blank_value);
+	template<> void clear_texture_glm(Texture1D& texture, glm::ivec3 offset, glm::ivec3 size, glm::vec4 color) {
+		texture.clear(color, offset.x, size.x, 0);
+	}
+	template<> void clear_texture_glm(Texture2D& texture, glm::ivec3 offset, glm::ivec3 size, glm::vec4 color) {
+		texture.clear(color, offset.x, offset.y, size.x, size.y, 0);
+	}
+	template<> void clear_texture_glm(Texture2DArray& texture, glm::ivec3 offset, glm::ivec3 size, glm::vec4 color) {
+		texture.clear(color, offset.x, offset.y, offset.z, size.x, size.y, size.z, 0);
+	}
+	template<> void clear_texture_glm(Texture3D& texture, glm::ivec3 offset, glm::ivec3 size, glm::vec4 color) {
+		texture.clear(color, offset.x, offset.y, offset.z, size.x, size.y, size.z, 0);
 	}
 }
 
@@ -84,15 +101,15 @@ template<typename T>
 inline std::shared_ptr<T> FFFT2::create(T& source, component comp, glm::ivec3 size)
 {
 	Texture2D::ColorTextureFormat format = source.get_internal_format_color();
-	if (comp == real_complex)	format = complex_texture_format(format);
 	if (comp == real)			format = real_texture_format(format);
 	if (comp == complex)		format = complex_texture_format(format);
+	if (comp == real_complex)	format = complex_texture_format(format);
 
 	if (size.x <= 0) size.x = to_ivec3(source.get_size()).x;
 	if (size.y <= 0) size.y = to_ivec3(source.get_size()).y;
 	if (size.z <= 0) size.z = to_ivec3(source.get_size()).z;
 
-	return create_texture_glm<T>(source.get_size(), format);
+	return create_texture_glm<T>(to_ivec3(source.get_size()), format);
 }
 
 template<typename T>
@@ -114,6 +131,11 @@ inline void FFFT2::copy(T& source, T& target, component comp, glm::ivec3 source_
 		ASSERT(false);
 	}
 
+	if (is_real(source) && is_real(target) && comp == real)
+		comp = real_complex;
+
+	if (is_real(source) && is_real(target) && comp == complex)
+		return;
 
 	if (size.x == 0) size.x = to_ivec3(glm::min(source.get_size(), target.get_size()), 1).x - max(source_offset, target_offset).x;
 	if (size.y == 0) size.y = to_ivec3(glm::min(source.get_size(), target.get_size()), 1).y - max(source_offset, target_offset).y;
@@ -135,17 +157,7 @@ inline void FFFT2::copy(T& source, T& target, component comp, glm::ivec3 source_
 	cp_copy.variant_define("target_image",			T::ColorTextureFormat_to_OpenGL_compute_Image_type(target.get_internal_format_color(), TextureBase2::get_texture_dimention<T>()));
 	cp_copy.variant_define("source_image_dimensionality",		std::to_string(TextureBase2::get_texture_dimention<T>()));
 	cp_copy.variant_define("target_image_dimensionality",		std::to_string(TextureBase2::get_texture_dimention<T>()));
-	
-	component source_component = 
-		is_real(source)		? real : 
-		is_complex(source)	? complex : real_complex;
 
-	component target_component =
-		is_real(target)		? real :
-		is_complex(target)	? complex : real_complex;
-
-	cp_copy.variant_define("source_number_type",	component_to_string(source_component));
-	cp_copy.variant_define("target_number_type"	,	component_to_string(target_component));
 	cp_copy.variant_define("copy_operation",		component_to_string(comp));
 	
 	ComputeProgram& kernel = *cp_copy.get_current_variant();
@@ -161,4 +173,68 @@ inline void FFFT2::copy(T& source, T& target, component comp, glm::ivec3 source_
 	kernel.update_uniform("fft_texture_region", size);
 
 	kernel.dispatch_thread(size);
+}
+
+template<typename T>
+inline void FFFT2::pad(T& source, T& target, glm::vec2 padding_value, glm::ivec3 offset)
+{
+	if (glm::any(glm::lessThan(offset, glm::ivec3(0)))) {
+		std::cout << "[FFFT Error] FFFT::pad() is called with an negative offset" << std::endl;
+		ASSERT(false);
+	}
+
+	if (glm::any(glm::greaterThan(to_ivec3(source.get_size()) + offset, to_ivec3(target.get_size())))) {
+		std::cout << "[FFFT Error] FFFT::pad() is called but specified offset + source's size exceeds target's size" << std::endl;
+		ASSERT(false);
+	}
+
+	clear_texture_glm(target, glm::ivec3(0), to_ivec3(target.get_size()), glm::vec4(padding_value, 0, 1));
+	copy(source, target, FFFT2::real_complex, glm::ivec3(0), offset, to_ivec3(source.get_size()));
+}
+
+template<typename T>
+inline void FFFT2::i_pad(T& source, T& target, glm::ivec3 offset)
+{
+	if (glm::any(glm::lessThan(offset, glm::ivec3(0)))) {
+		std::cout << "[FFFT Error] FFFT::i_pad() is called with an negative offset" << std::endl;
+		ASSERT(false);
+	}
+
+	if (glm::any(glm::greaterThan(to_ivec3(target.get_size()) + offset, to_ivec3(source.get_size())))) {
+		std::cout << "[FFFT Error] FFFT::i_pad() is called but specified offset + target's size exceeds source's size" << std::endl;
+		ASSERT(false);
+	}
+
+	copy(source, target, FFFT2::real_complex, offset, glm::ivec3(0), to_ivec3(target.get_size()));
+}
+
+template<typename T>
+inline std::shared_ptr<T> FFFT2::pad(T& source, glm::vec2 padding_value, glm::ivec3 offset)
+{
+	if (glm::any(glm::lessThan(offset, glm::ivec3(0)))) {
+		std::cout << "[FFFT Error] FFFT::pad() is called with negative size value" << std::endl;
+		ASSERT(false);
+	}
+
+	glm::ivec3 final_size = to_ivec3(source.get_size()) + offset;
+	std::shared_ptr<T> target = create_texture_glm<T>(final_size, source.get_internal_format_color());
+
+	pad(source, *target, padding_value, offset);
+
+	return target;
+}
+
+template<typename T>
+inline std::shared_ptr<T> FFFT2::i_pad(T& source, glm::ivec3 offset, glm::ivec3 size)
+{
+	if (glm::any(glm::lessThan(offset, glm::ivec3(0))) || glm::any(glm::lessThan(size, glm::ivec3(0)))) {
+		std::cout << "[FFFT Error] FFFT::i_pad() is called with negative offset or size values" << std::endl;
+		ASSERT(false);
+	}
+
+	std::shared_ptr<T> target = create_texture_glm<T>(size, source.get_internal_format_color());
+
+	i_pad(source, *target, offset);
+
+	return target;
 }
